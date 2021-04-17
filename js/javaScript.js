98f65035d73ae99334cf4471e6552cc72b0d2d64
abdelhakim tamoum <tamoum.hakim@gmail.com>
2021-04-17 15:32:21 -0400

    first commit

---
+++ js/javaScript.js
@@ -0,0 +1,80 @@
+$(function() { 
+    "use strict";
+    
+    // scroll plugin
+    $("html").niceScroll(
+    {cursorcolor: "#f7600e",cursorborder: "1px solid #f7600e"
+    });
+    
+    // header height
+    $('.header').height($(window).height());
+    
+    // scroll to feactures
+    $('.header .arrow i').click( function() {
+        $('html, body').animate({ scrollTop: $('.features').offset().top}, 700);
+    });
+    
+    // show hidden
+    $('.show-more').click(function(){
+        $('.item-box .hidden').slideDown(700);   
+    });
+    
+    // testimonials
+    var right = $('.testimonials .fa-chevron-right'),
+        left = $('.testimonials .fa-chevron-left');
+    function checkClients () {
+       
+        $('.client:first').hasClass('active') ? left.fadeOut() : left.fadeIn();
+        $('.client:last').hasClass('active') ? right.fadeOut() : right.fadeIn();
+    }
+    checkClients();
+    
+    $('.testimonials i').click(function () {
+        if ($(this).hasClass('fa-chevron-right')) {
+            $('.testimonials .active').fadeOut(400, function() {
+               $(this).removeClass('active').next('.client').addClass('active').fadeIn();
+                checkClients();
+            });
+        }else {
+             $('.testimonials .active').fadeOut(400, function() {
+               $(this).removeClass('active').prev('.client').addClass('active').fadeIn();
+                checkClients();
+            });
+        }
+    });
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
